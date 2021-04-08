d4e7e9e18669c2ae1ef1b10dd1adadedd1f36e77
Gaven <gavengrantz@gmail.com>
2021-04-07 21:53:08 -0700

    Api stuff

--- game-app/src/App.js
+++ game-app/src/App.js
@@ -1,6 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+const axios = require('axios').default;
+axios.post('https://id.twitch.tv/oauth2/token', null, {
+  params: {
+    client_id: '',
+    client_secret: '',
+    grant_type: 'client_credentials'
+  }
+})
+  .then(function (response) {
+    console.log(response)
+    console.log("before igdb post " + response.data.access_token)
+    const body = "fields name, rating, total_rating;where total_rating > 90 & release_dates.date > 1609459231;sort total_rating desc;limit 10;"
+    axios.post('https://api.igdb.com/v4/games', body, {
+      headers: {
+        'Client-ID': '',
+        'Authorization': 'Bearer ' + response.data.access_token,
+        'Content-Type': 'text/plain'
+      }
+    })
+      .then(function (response) {
+        console.log(response)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+      console.log("after igdb post " + response.data.access_token)
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
+
+
+
+
+
+
+
+
+
+
+
 function App() {
   return (
     <div className="App">
