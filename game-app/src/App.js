4bac5b2b5f4150206614fb3fbdd36f8d393742af
Daniel <dpalencia2112@gmail.com>
2021-04-13 14:06:51 -0700

    #2 Refactor API code into class. Add game data to landing page

--- game-app/src/App.js
+++ game-app/src/App.js
@@ -1,69 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import APIUtility from './utils/APIUtility.js';
+import React, { Component } from "react";
 
-
-
-const axios = require('axios').default;
-axios.post('https://id.twitch.tv/oauth2/token', null, {
-  params: {
-    client_id: '',
-    client_secret: '',
-    grant_type: 'client_credentials'
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { top10games: []}
   }
-})
-  .then(function (response) {
-    console.log(response)
-    console.log("before igdb post " + response.data.access_token)
-    const body = "fields name, rating, total_rating;where total_rating > 90 & release_dates.date > 1609459231;sort total_rating desc;limit 10;"
-    axios.post('https://api.igdb.com/v4/games', body, {
-      headers: {
-        'Client-ID': '',
-        'Authorization': 'Bearer ' + response.data.access_token,
-        'Content-Type': 'text/plain'
-      }
-    })
-      .then(function (response) {
-        console.log(response)
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
-      console.log("after igdb post " + response.data.access_token)
-  })
-  .catch(function (error) {
-    console.log(error)
-  })
 
+  componentDidMount() {
+    let apiUtil = new APIUtility();
+    apiUtil.getTop10Games().then(response => this.setState({top10games: response}))
+  }
 
-
-
-
-
-
-
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World! I'm supposed to be a game website soon.</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello World! I'm supposed to be a game website soon.</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          {this.state.top10games.map(game => 
+              <li>
+                {game.name}
+              </li>
+          )}
+        </header>
+      </div>
+    );
+  }
 }
+
 
 export default App;
