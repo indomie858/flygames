22673fe906e7436e49ca030a80b09e92a0604c24
Gaven <gavengrantz@gmail.com>
2021-05-12 15:08:46 -0700

    Added black background to carosel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Spinner from "../Components/Spinner";
 
 export const StyledCarousel = styled.div`
+    background: black;
     max-width: 1280px;
     margin: 0 auto;
     position: relative;
