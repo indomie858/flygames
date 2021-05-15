d82e99701e15c1998b75c44c17c7203dc99ad4fd
Gaven <gavengrantz@gmail.com>
2021-05-14 21:46:12 -0700

    fixed release dates

--- src/Pages/GamePage.js
+++ src/Pages/GamePage.js
@@ -36,17 +36,17 @@ const GamePage = ({ gameID }) => {
       let response = await apiUtil.getGameInfo(gameID)
       setLoading(true)
       setGameInfo(response[0])
-      
+
       //let companyIDs = response[0].involved_companies.map()
       let companyObjects = await apiUtil.getCompanyInfoFromIDArray(response[0].involved_companies)
       setCompanies(companyObjects)
 
       let similarGames = await apiUtil.getGamesByIDs(response[0].similar_games)
       setSimilarGames(similarGames)
-      
+
       setLoading(false)
     }
-    requestGameInfo() 
+    requestGameInfo()
   }, [gameID])
 
   const getCoverSizeBig = (imageURL) => {
@@ -59,55 +59,54 @@ const GamePage = ({ gameID }) => {
 
   function gameInfoMain() {
     return <>
-          <Carousel screenshots={gameInfo.screenshots} />
-          <main className="main-container">
-          <div class="horizontal-flex">
-              <div class="flex-left pad-24">
-                <h1 class="text-center">
-                  {gameInfo.name}
-                </h1>
-                <div class="flex-justify-center">
-                  { gameInfo.hasOwnProperty("genres") ? gameInfo.genres.map((genre) => <Link to={`/searchResults/bygenre/${genre.name}`}><StyledListItem>{genre.name}</StyledListItem></Link>) : null }
-                </div>
-                <div>
-                  <p class="text-justify">
-                    {gameInfo.summary}
-                  </p>
-                </div>
-                <Rating rating={Math.round(gameInfo.total_rating)}></Rating>
-              </div>
-              <div class="flex-right pad-24">
-                  <h4>Publishers & Developers</h4>
-                    <div>
-                      { companies == undefined ? null : companies.map((company) => <StyledListItem>{company.name}</StyledListItem>)}
-                    </div>
-                  <h4>Release Date</h4>
-                    <StyledDate>{gameInfo.release_dates[0].human}</StyledDate>
-                    {gameInfo.release_dates == undefined ? null : <StyledDate>{gameInfo.release_dates[0].human}</StyledDate>}
-                  <h4>Platforms</h4>
-                    {gameInfo.platforms == null ? null : gameInfo.platforms.map((platform) => <StyledListItem>{platform.abbreviation}</StyledListItem>)}
+      <Carousel screenshots={gameInfo.screenshots} />
+      <main className="main-container">
+        <div class="horizontal-flex">
+          <div class="flex-left pad-24">
+            <h1 class="text-center">
+              {gameInfo.name}
+            </h1>
+            <div class="flex-justify-center">
+              {gameInfo.hasOwnProperty("genres") ? gameInfo.genres.map((genre) => <Link to={`/searchResults/bygenre/${genre.name}`}><StyledListItem>{genre.name}</StyledListItem></Link>) : null}
             </div>
+            <div>
+              <p class="text-justify">
+                {gameInfo.summary}
+              </p>
             </div>
-            <div class="similar-games">
-              <h3>Similar Games</h3>
-              <div>
-                <div className="grid">
-                  { similarGames == null ? null : similarGames.map((game) => (
-                      <GameCard
-                        gameName={game.name}
-                        imageUrl={getCoverSizeBig(game.cover ? game.cover.url : `${NoImage}`)}
-                        gameRating={game.rating}
-                        gameID={game.id}
-                      />
-                    ))}
-                </div>
-              </div>
+            <Rating rating={Math.round(gameInfo.total_rating)}></Rating>
+          </div>
+          <div class="flex-right pad-24">
+            <h4>Publishers & Developers</h4>
+            <div>
+              {companies == undefined ? null : companies.map((company) => <StyledListItem>{company.name}</StyledListItem>)}
             </div>
-          </main>
-        </>
+            <h4>Release Date</h4>
+            {gameInfo.release_dates == undefined ? null : <StyledDate>{gameInfo.release_dates[0].human}</StyledDate>}
+            <h4>Platforms</h4>
+            {gameInfo.platforms == null ? null : gameInfo.platforms.map((platform) => <StyledListItem>{platform.abbreviation}</StyledListItem>)}
+          </div>
+        </div>
+        <div class="similar-games">
+          <h3>Similar Games</h3>
+          <div>
+            <div className="grid">
+              {similarGames == null ? null : similarGames.map((game) => (
+                <GameCard
+                  gameName={game.name}
+                  imageUrl={getCoverSizeBig(game.cover ? game.cover.url : `${NoImage}`)}
+                  gameRating={game.rating}
+                  gameID={game.id}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
   }
 
-  return ( loading ? <Spinner /> : gameInfoMain() );
+  return (loading ? <Spinner /> : gameInfoMain());
 };
 
 export default GamePage;
